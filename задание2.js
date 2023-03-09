07150670ca18ee90f4a65a9734b9e190bf4e7dee
Johanna-2206 <126691370+Johanna-2206@users.noreply.github.com>
2023-03-10 02:08:11 +0700

    Файлы из раздела JS
    Добавление файлов из первой самостоятельной работы по JavaScript

---
+++ задание2.js
@@ -0,0 +1,8 @@
+{
+    const speed1=40;
+    const speed2=45;
+    let distance=1000;
+    let time=2;
+    let distance2=distance-speed1*time-speed2*time;
+    console.log(distance2);
+}
---
+++ задание3.js
@@ -0,0 +1,9 @@
+{
+    const edge =15;
+    let Sгрань=edge**2;
+    let Sкуб=6*edge**2;
+    let Vкуб=edge**3;
+    console.log(Sгрань);
+    console.log(Sкуб);
+    console.log(Vкуб);
+}
